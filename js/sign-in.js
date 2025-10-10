e52ca30d8c97211f1c3be89f7fb2fd4d2790fced
BearCubsYT <elitebearcubsyt@gmail.com>
2025-10-10 09:17:04 -0400

    Started Sign In/Sign Up

---
+++ js/sign-in.js
@@ -0,0 +1,3 @@
+document.getElementById("sign-in-button").addEventListener('click', () => {
+    console.log("Sign In")
+});
---
+++ js/sign-up.js
@@ -0,0 +1,3 @@
+document.getElementById("sign-up-button").addEventListener('click', () => {
+    console.log("Sign Up")
+});
