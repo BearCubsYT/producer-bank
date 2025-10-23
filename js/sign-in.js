19c080fa7283e3173999ffab2e953f71122ce462
BearCubsYT <elitebearcubsyt@gmail.com>
2025-10-23 10:38:17 -0400

    Fixed issues

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,20 +1,21 @@
-async function fetchDataAndUse() {
-    fetch('./../data/users.json')
+function fetchDataAndUse() {
+    return fetch('./../data/users.json')
     .then(response => response.json())
     .then(jsonData => {
-        return jsonData.value;
+        return jsonData;
     })
     .catch(error => console.error('Error loading JSON:', error));
 }
 
-document.getElementById("sign-in").addEventListener('click', () => {
+document.getElementById("sign-in").addEventListener('click', async () => {
     if (document.getElementById("username").value != "" && document.getElementById("password").value != "") {
-        let usersData = fetchDataAndUse();
-        console.log(usersData);
-        // for (let user in usersData) {
-        //     if (user == document.getElementById("username").value){
-        //             console.log(user);
-        //     };
-        // };
+        let usersData = await fetchDataAndUse();
+        for (let user in usersData) {
+            if (user == document.getElementById("username").value){
+                
+            } else {
+                document.getElementById("incorrect") = "Incorrect Username and/or Password";
+            };
+        };
     };
 });
