d5c3091467ff7f3ee3fd1f3f87d5c7417e5e9601
Jay <151044640+BearCubsYT@users.noreply.github.com>
2025-10-24 07:46:37 -0400

    Not even clse to done, I quit

--- js/sign-in.js
+++ js/sign-in.js
@@ -7,15 +7,29 @@ function fetchDataAndUse() {
     .catch(error => console.error('Error loading JSON:', error));
 }
 
+// If user presses login button
 document.getElementById("sign-in").addEventListener('click', async () => {
+    // Resets the incorrect text to blank incase they no longer are inputting wrong info
+    document.getElementById("incorrect").innerHTML = "";
+    // Makes sure they dont have blank inputs
     if (document.getElementById("username").value != "" && document.getElementById("password").value != "") {
         let usersData = await fetchDataAndUse();
-        for (let user in usersData) {
+        for (user in usersData) {
+            // Compare inputted username to all usernames in database
             if (user == document.getElementById("username").value){
-                
+                // If the account wwas foun mak sure the password is correct
+                if (user["password"] == document.getElementById("password").value) {
+                    // let generalData = await fetchDataAndUse2();
+                    // generalData["Logged In"] = true;
+                    // window.open('./../html/index.html');
+                } else {
+                    // Set incorrect text section to say they put wrong password in
+                    document.getElementById("incorrect").innerHTML = "Incorrect Password";
+                }
             } else {
-                document.getElementById("incorrect") = "Incorrect Username and/or Password";
+                // Set incorrect text section to say they put wrong username and/or password in
+                document.getElementById("incorrect").innerHTML = "Incorrect Username and/or Password";
             };
-        };
+        }
     };
 });
